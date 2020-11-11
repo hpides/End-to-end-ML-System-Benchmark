bbd04bf5d313f97cdd057bc8459457d514d1e996
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-11-11 21:59:57 +0100

    renaming and restructuring in feature_extraction_pandas.py, add .idea file to .gitignore, add target argument to feature_extraction_pandas.py

--- src/feature_extraction/feature_extraction_pandas.py
+++ src/feature_extraction/feature_extraction_pandas.py
@@ -5,8 +5,10 @@ import torch
 import re
 
 parser = argparse.ArgumentParser()
-parser.add_argument('-s', dest='rows_in_memory', type=int, default=None,
-                    help="maximum amount of rows that is kept in memory at any one time")
+parser.add_argument('-s', '--size', dest='rows_in_memory', type=int, default=None,
+                    help="maximum amount of rows that is kept in memory at any time, None if no such limitation")
+parser.add_argument('-t', '--target', dest='target', default='pytorch', choices=['pytorch', 'numpy', 'tensorflow'],
+                    help='format of saved feature tensors')
 args = parser.parse_args()
 
 raw_data_path = "../../data/raw"
@@ -16,33 +18,33 @@ def feature_extract(df):
     return df[['smart_5_normalized', 'smart_187_normalized', 'smart_188_normalized', 'smart_197_normalized',
         'smart_198_normalized']]
 
-def save_as_pytorch(df, path):
-    torch.save(torch.tensor(feature_extract(df).values), path)
+def save_features_to_file(df, write_path):
+    if args.target == 'pytorch':
+        torch.save(torch.tensor(feature_extract(df).values), write_path)
 
-def parse_file(input_path, df_to_feature_file):
-    base_filename = re.findall(r"/([^/]*)\.csv", input_path)[0]
-    feature_file_path = os.path.join(features_path, base_filename)
-    if args.rows_in_memory is None:
-        df = pd.read_csv(input_path)
-        df_to_feature_file(df, feature_file_path)
-    else:
-        skiprows = 1
-        still_rows_left = True
-        output_file_suffix = 0
-        while still_rows_left:
-            df = pd.read_csv(input_path, skiprows=range(1, skiprows), nrows=args.rows_in_memory)
-            skiprows += args.rows_in_memory
-            if len(df) < args.rows_in_memory:
-                still_rows_left = False
-            feature_file_path = os.path.join(features_path, f"{base_filename}_{output_file_suffix}")
-            df_to_feature_file(df, feature_file_path)
-            output_file_suffix += 1
-
-def main():
+def parse_and_save_files():
     os.mkdir(features_path)
-    for filename in os.listdir(raw_data_path):
-        parse_file(os.path.join(raw_data_path, filename), save_as_pytorch)
+    for read_filename in os.listdir(raw_data_path):
+        read_filepath = os.path.join(raw_data_path, read_filename)
+        read_file_base_name = re.findall(r"(.*)\.csv", read_filename)[0]
 
+        if args.rows_in_memory is None:
+            write_filepath = os.path.join(features_path, read_file_base_name)
+            df = pd.read_csv(read_filepath)
+            save_features_to_file(df, write_filepath)
+        else:
+            skiprows = 1
+            rows_left_to_read = True
+            output_file_suffix = 0
+            while rows_left_to_read:
+                df = pd.read_csv(read_filepath, skiprows=range(1, skiprows), nrows=args.rows_in_memory)
+                skiprows += args.rows_in_memory
+                if len(df) < args.rows_in_memory:
+                    rows_left_to_read = False
+                write_filename = f"{read_file_base_name}_{output_file_suffix}"
+                write_filepath = os.path.join(features_path, write_filename)
+                save_features_to_file(df, write_filepath)
+                output_file_suffix += 1
 
 if __name__ == "__main__":
-    main()
+    parse_and_save_files()
