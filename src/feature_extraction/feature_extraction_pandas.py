df3bdf616bf3f883bdc98ae3681ae047fb3032a0
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-11-16 15:39:46 +0100

    write normalized features to hd5

--- src/feature_extraction/feature_extraction_pandas.py
+++ src/feature_extraction/feature_extraction_pandas.py
@@ -5,50 +5,38 @@ import torch
 import re
 import numpy as np
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--size', dest='rows_in_memory', type=int, default=None,
-                    help="maximum amount of rows that is kept in memory at any time, None if no such limitation")
-parser.add_argument('-t', '--target', dest='target', default='numpy', choices=['pytorch', 'numpy'],
-                    help='format of saved feature tensors')
-args = parser.parse_args()
-
 raw_data_path = "../../data/raw"
-features_path = "../../data/features"
+features_path = "../../data/features.h5"
+labels_path = "../../data/labels.h5"
+
+smart_values = ['smart_197_raw', 'smart_9_raw', 'smart_241_raw', 'smart_187_raw']
 
 def feature_extract(df):
-    return df[['smart_5_normalized', 'smart_187_normalized', 'smart_188_normalized', 'smart_197_normalized',
-        'smart_198_normalized']]
+    return df[smart_values].astype('float'), df['failure']
 
-def save_features_to_file(df, write_path):
-    if args.target == 'pytorch':
-        torch.save(torch.tensor(feature_extract(df).values), write_path)
-    if args.target == 'numpy':
-        np.save(write_path, feature_extract(df).values)
-
-
-def parse_and_save_files():
-    os.mkdir(features_path)
-    for read_filename in os.listdir(raw_data_path):
+def write_feature_columns_to_hdf():
+    for i, read_filename in enumerate(os.listdir(raw_data_path)):
+        print(f"Parsing file {i} out of {len(os.listdir(raw_data_path))}")
         read_filepath = os.path.join(raw_data_path, read_filename)
-        read_file_base_name = re.findall(r"(.*)\.csv", read_filename)[0]
+        df = pd.read_csv(read_filepath)
+        X, y = feature_extract(df)
+        X.to_hdf(features_path, key='df', append=True, mode='a')
+        y.to_hdf(labels_path, key='df', append=True, mode='a')
 
-        if args.rows_in_memory is None:
-            write_filepath = os.path.join(features_path, read_file_base_name)
-            df = pd.read_csv(read_filepath)
-            save_features_to_file(df, write_filepath)
-        else:
-            skiprows = 1
-            rows_left_to_read = True
-            output_file_suffix = 0
-            while rows_left_to_read:
-                df = pd.read_csv(read_filepath, skiprows=range(1, skiprows), nrows=args.rows_in_memory)
-                skiprows += args.rows_in_memory
-                if len(df) < args.rows_in_memory:
-                    rows_left_to_read = False
-                write_filename = f"{read_file_base_name}_{output_file_suffix}"
-                write_filepath = os.path.join(features_path, write_filename)
-                save_features_to_file(df, write_filepath)
-                output_file_suffix += 1
+def normalize_hdf_columns():
+    features = pd.read_hdf(features_path)
+
+    col_means = features.mean(axis=0)
+    col_stddevs = features.std(axis=0)
+
+    #replace nan values with column means
+    features.fillna(col_means, inplace=True)
+
+    #normalize to standard normal distribution
+    features = (features - col_means) / col_stddevs
+
+    features.to_hdf(features_path, key='df', mode='a', append=True)
 
 if __name__ == "__main__":
-    parse_and_save_files()
+    write_feature_columns_to_hdf()
+    normalize_hdf_columns()
