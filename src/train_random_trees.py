5a8dc15ab834892836ef416e669b5108a904280b
jostmorgenstern <Jost.Morgenstern@student.hpi.uni-potsdam.de>
2020-11-30 14:12:49 +0100

    add min_samples_split parameter to RandomForestClassifier

--- src/train_random_trees.py
+++ src/train_random_trees.py
@@ -16,7 +16,7 @@ def train_and_test():
         print(Counter(y_train))
         print(Counter(y_test))
 
-        classifier = RandomForestClassifier(random_state=0)
+        classifier = RandomForestClassifier(n_estimators=10, max_depth=2, min_samples_split=100, random_state=0)
         classifier.fit(X_train, y_train)
         plot_confusion_matrix(classifier, X_test, y_test)
         plt.show()
