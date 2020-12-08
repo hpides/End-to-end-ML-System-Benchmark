e2f3c9f469ff602b38f55f7e7a09aff043660f6c
Vaskulis <vaskulis@posteo.de>
2020-12-08 11:58:39 +0100

    added inheritance to decorators

--- src/decorators.py
+++ src/decorators.py
@@ -1,9 +1,20 @@
 import time
+from datetime import datetime
 
 
-class MeasureTime(object):
+class Measure(object):
+    name = None
+
     def __init__(self, output_file):
         self.output_file = output_file
+
+    def log(self, function_name, value):
+        with open(self.output_file, 'a+') as out_file:
+            out_file.write(f"{str(datetime.now())} --- {function_name} --- {self.name}: {value}\n")
+
+
+class MeasureTime(Measure):
+    name = "Time measurement"
 
     def __call__(self, func):
         def inner(*args, **kwargs):
@@ -11,7 +22,6 @@ class MeasureTime(object):
             result = func(*args, **kwargs)
             after = time.perf_counter()
             time_taken = after - before
-            with open(self.output_file, 'a+') as out_file:
-                out_file.write(f"Time taken: {time_taken} \n")
+            self.log(func.__name__, time_taken)
             return result
         return inner
