5392ce8e37489c2494f5ae3114202d6843c58857
sjoze <49590442+sjoze@users.noreply.github.com>
2021-01-25 14:11:38 +0100

    Update main.py
    added decorators

--- so2sat_pipeline/main.py
+++ so2sat_pipeline/main.py
@@ -1,9 +1,11 @@
 import train_so2sat
 import test_so2sat
+from benchmarking import bm
 
 def main():
-    model = train_so2sat.train()
-    test_so2sat.test(model)
+    result = train_so2sat.train()
+    test_so2sat.test(result["model"])
+    bm.close()
 
 if __name__ == "__main__":
     main()
