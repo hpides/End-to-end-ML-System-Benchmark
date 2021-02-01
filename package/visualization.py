bb4ad0a002120be8ecbc6d6738e71586785703c1
Christian Jacob <ccj.99@t-online.de>
2021-02-01 12:51:49 +0100

    Added visualization for Time, Throughput und Latency

--- package/visualization.py
+++ package/visualization.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine, desc, asc
 
 import pandas as pd
 import seaborn as sn
+import numpy as np
 
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
@@ -19,6 +20,9 @@ def visualize(uuid):
         df_dict = make_dataframe_from_database(uuid, session)
         plot_measurement_type(df_dict, "Memory")
         plot_confusion_matrix(df_dict)
+        plot_time(df_dict)
+        plot_throughput(df_dict)
+        plot_latency(df_dict)
 
         session.commit()
     except:
@@ -58,8 +62,8 @@ def plot_measurement_type(df, measurement_type):
     ax.yaxis.set_major_locator(ticker.LinearLocator(12))
     plt.show()
 
-def plot_confusion_matrix(df):
 
+def plot_confusion_matrix(df):
     values = df.loc[df.measurement_type == "Multiclass Confusion Matrix"].value.values
     classes = df.loc[df.measurement_type == "Multiclass Confusion Matrix Class"].value.values
 
@@ -68,7 +72,7 @@ def plot_confusion_matrix(df):
     for i in range(len(classes)):
         con_mat_row = []
         for j in range(len(classes)):
-            con_mat_row.append(int(values[j+i*len(classes)]))
+            con_mat_row.append(int(values[j + i * len(classes)]))
         con_mat.append(con_mat_row)
 
     matrix = pd.DataFrame(con_mat, index=classes, columns=classes)
@@ -78,6 +82,71 @@ def plot_confusion_matrix(df):
     plt.show()
 
 
+def plot_time(df):
+    values = df.loc[df.measurement_type == "Time"].value.values
+    label = df.loc[df.measurement_type == "Time"].description.values
+
+    df = pd.DataFrame(
+        dict(zip(label, values.astype(np.float))),
+        index={"Runtime"}
+    )
+
+    ax = df.plot.barh(stacked=True)
+    plt.title("Time spent in phases")
+    plt.xlabel("Time in seconds")
+    x_offset = 0
+    y_offset = 0.02
+    for p in ax.patches:
+        b = p.get_bbox()
+        print(b)
+        val = "{:.2f}".format(b.x1 - b.x0)
+        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
+    plt.show()
+
+
+def plot_throughput(df):
+    values = df.loc[df.measurement_type == "Throughput"].value.values
+    label = df.loc[df.measurement_type == "Throughput"].description.values
+
+    df = pd.DataFrame(
+        dict(zip(label, values.astype(np.float))),
+        index={"Throughput"}
+    )
+    ax = df.plot.barh()
+    plt.title("Throughput")
+    plt.xlabel("Seconds per entry")
+    x_offset = 0
+    y_offset = 0.02
+    for p in ax.patches:
+        b = p.get_bbox()
+        print(b)
+        val = "{:.2f}".format(b.x1 - b.x0)
+        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
+    plt.show()
+
+
+def plot_latency(df):
+    values = df.loc[df.measurement_type == "Latency"].value.values
+    label = df.loc[df.measurement_type == "Latency"].description.values
+
+    df = pd.DataFrame(
+        dict(zip(label, values.astype(np.float))),
+        index={"Latency"}
+    )
+    ax = df.plot.barh()
+    plt.title("Latency")
+    plt.xlabel("Entries per second")
+    x_offset = 0
+    y_offset = 0.02
+    for p in ax.patches:
+        b = p.get_bbox()
+        print(b)
+        val = "{:.2f}".format(b.x1 - b.x0)
+        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
+    plt.show()
+
+
 if __name__ == "__main__":
-    ## visualize('8a989821-f2e9-48da-ab41-36cb0fc6f580')
-    visualize("a8c2115c-0d6a-4cbc-ad47-445289d136fc")
+    ## visualize('8a989821-f2e9-48da-ab41-36cb0fc6f580') ## Willi
+    ## visualize("a8c2115c-0d6a-4cbc-ad47-445289d136fc") ## Jonas
+    visualize("e9bc18ae-eb3e-4268-beef-0c6e8e43c00a")  ## Christian
