843390658a531516a0d17701045c904fa26ab6ab
sjoze <49590442+sjoze@users.noreply.github.com>
2021-02-23 15:16:16 +0100

    Cleaned up. Metric detection per uuid
    Can now detect which metrics were used in which run. Given multiple (or single) uuid it will now decide which metrics are being visualized with which runs.

--- package/visualization.py
+++ package/visualization.py
@@ -6,22 +6,24 @@ import seaborn as sn
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
-from sklearn.metrics import plot_confusion_matrix
 
 
 def visualize(uuids, database_file):
     engine = create_engine(f'sqlite+pysqlite:///{database_file}')
-    # engine = create_engine('sqlite+pysqlite:///so2sat_benchmark.db')
     Session = sessionmaker(bind=engine)
     session = Session()
     try:
-        df_dict = make_dataframe_from_database_mult(uuids, session)
-        plot_TTA(df_dict)
-        plot_measurement_type(df_dict, "Memory (psutil)")
-        plot_confusion_matrix(df_dict)
-        plot_time(df_dict)
-        plot_throughput(df_dict)
-        plot_latency(df_dict)
+        df = make_dataframe_from_database(uuids, session)
+        metrics = df.measurement_type.unique()
+
+        for metric in metrics:
+            if metric == "Multiclass Confusion Matrix Class":           # helper class for MCCM, not a metric on its own
+                continue
+            for uuid in uuids:
+                filtered_df = df.loc[(df.measurement_type == metric) & (df.uuid == uuid)]
+                if len(filtered_df) != 0:
+                    metrics_dict[metric](df)
+                    break
 
         session.commit()
     except:
@@ -31,23 +33,9 @@ def visualize(uuids, database_file):
         session.close()
 
 
-def make_dataframe_from_database(uuid, session):
-    measure_query = session.query(Measurement.benchmark_uuid,
-                                  Measurement.datetime,
-                                  Measurement.description,
-                                  Measurement.measurement_type,
-                                  Measurement.value,
-                                  Measurement.unit)
-    measure_query = measure_query.filter_by(benchmark_uuid=uuid).order_by(asc(Measurement.datetime))
-
-    df = pd.DataFrame(measure_query.all(), columns=["uuid", "datetime", "description", "measurement_type", "value",
-                                                    "unit"])
-
-    return df
-
-
 # visualize multiple uuids, also works with only a single one
-def make_dataframe_from_database_mult(uuids, session):
+def make_dataframe_from_database(uuids, session):
+
     measure_query = session.query(Measurement.benchmark_uuid,
                                   Measurement.datetime,
                                   Measurement.description,
@@ -66,32 +54,65 @@ def make_dataframe_from_database_mult(uuids, session):
     return df
 
 
-def plot_measurement_type(df, measurement_type):
+def plot_time_based_graph(df, measurement_type, title, xlabel, ylabel):
+
     fig = plt.figure(figsize=(12, 8))
     ax = fig.add_subplot(111)
 
-    for description in df.description.unique():
-        description_df = df.loc[(df.measurement_type == measurement_type) & (df.description == description)]
-        ax.plot(description_df.datetime.values,
-                description_df.value.values,
-                label=description)
+    filtered_df = df.loc[(df.measurement_type == measurement_type)]
 
-    ax.set_ylabel("MB of Memory")
-    ax.set_xlabel("Time")
+    for uuid in filtered_df.uuid.unique():
+        filtered_uuid_df = filtered_df.loc[(df.uuid == uuid)]
+        lastTimestamp, lastValue = 0, 0
+        for description in df.description.unique():
+            description_df = filtered_uuid_df.loc[df.description == description]
+            if len(description_df) != 0:
+                dates = description_df.datetime.values
+                duration = round((dates[len(dates) - 1] - dates[0]) / np.timedelta64(1, 's'))
+                if duration == 0:
+                    duration = 1.0
+                values = description_df.value.values.astype(float)
+                timestamps = []
+                if lastTimestamp != 0:
+                    timestamps.append(lastTimestamp)
+                    lastTimestamp += 1
+                    values = np.insert(values, 0, lastValue)
+                for i in range(len(dates)):
+                    timestamps.append(len(dates)*i/duration + lastTimestamp)
+                lastTimestamp = timestamps[len(timestamps)-1]
+                lastValue = values[-1]
+
+                ax.plot(timestamps,
+                        values,
+                        label=(uuid + ": " + description))
+
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
     plt.legend(loc=2)
+    plt.title(title)
 
     ax.yaxis.set_major_locator(ticker.LinearLocator(12))
     plt.show()
 
-    
+
+def plot_memory(df):
+
+    plot_time_based_graph(df, "Memory", "Memory usage", "Time in seconds", "MB used")
+
+
+def plot_energy(df):
+    plot_time_based_graph(df, "Energy", "Power consumption", "Time in seconds", "mJ of energy usage")
+
+
 def plot_TTA(df):
+
     fig = plt.figure(figsize=(12, 8))
     ax = fig.add_subplot(111)
 
     tta_df = df.loc[(df.measurement_type == "TTA")]
 
     for uuid in tta_df.uuid.unique():
-        tta_uuid_df = tta_df.loc[(df.uuid == str(uuid))]
+        tta_uuid_df = tta_df.loc[(df.uuid == uuid)]
         epochs = []
         for i in range(len(tta_uuid_df.value.values)):
             epochs.append(i+1)
@@ -109,119 +130,82 @@ def plot_TTA(df):
     plt.show()
 
 
-# currently need to cutoff first value due to first value always being 0
-# needs to be changed sometime in decorators.py
-def plot_energy(df):
-    fig = plt.figure(figsize=(12, 8))
-    ax = fig.add_subplot(111)
-
-    currentTimestamp = 0
-    for description in df.description.unique():
-        description_df = df.loc[(df.measurement_type == "Energy") & (df.description == description)]
-        if len(description_df) != 0:
-            duration = round((description_df.datetime.values[len(description_df.datetime.values) - 1] -
-                              description_df.datetime.values[0]) / np.timedelta64(1, 's'))
-            timevalues = []
-            for i in range(len(description_df.datetime.values)):
-                timevalues.append(len(description_df.datetime.values)*i/duration + currentTimestamp)
-            currentTimestamp = timevalues[len(timevalues)-1]
-            ax.plot(timevalues[1:len(description_df.datetime.values)],
-                    description_df.value.values.astype(float)[1:len(description_df.datetime.values)],
-                    label=description)
-
-    ax.set_ylabel("mJ of energy usage")
-    ax.set_xlabel("Time in seconds")
-    plt.legend(loc=2)
-    plt.title("Energy consumption")
-
-    ax.yaxis.set_major_locator(ticker.LinearLocator(12))
-    plt.show()    
-    
-
 def plot_confusion_matrix(df):
-    values = df.loc[df.measurement_type == "Multiclass Confusion Matrix"].value.values
-    classes = df.loc[df.measurement_type == "Multiclass Confusion Matrix Class"].value.values
 
-    con_mat = []
+    for uuid in df.uuid.unique():
 
-    for i in range(len(classes)):
-        con_mat_row = []
-        for j in range(len(classes)):
-            con_mat_row.append(int(values[j + i * len(classes)]))
-        con_mat.append(con_mat_row)
+        values = df.loc[(df.measurement_type == "Multiclass Confusion Matrix") & (df.uuid == uuid)].value.values
+        if len(values) == 0:
+            break
+        classes = df.loc[(df.measurement_type == "Multiclass Confusion Matrix Class") & (df.uuid == uuid)].value.values
 
-    matrix = pd.DataFrame(con_mat, index=classes, columns=classes)
-    plt.figure(figsize=(12, 8))
-    plt.title("Confusion Matrix")
-    sn.heatmap(matrix, annot=True)
+        con_mat = []
+
+        for i in range(len(classes)):
+            con_mat_row = []
+            for j in range(len(classes)):
+                con_mat_row.append(int(values[j + i * len(classes)]))
+            con_mat.append(con_mat_row)
+
+        matrix = pd.DataFrame(con_mat, index=classes, columns=classes)
+        plt.figure(figsize=(12, 8))
+        plt.title("Confusion Matrix for run " + uuid)
+        sn.heatmap(matrix, annot=True)
+        plt.show()
+
+
+def plot_barh(df, measurement_type, title, xlabel):
+
+    filtered_df = df.loc[df.measurement_type == measurement_type]
+    uuids = filtered_df.uuid.unique()
+    descriptions = filtered_df.description.unique()
+
+    dic = {"uuids": uuids}
+    for description in descriptions:
+        dic[description] = filtered_df.loc[filtered_df.description == description].value.values.astype(np.float)
+
+    df = pd.DataFrame(
+        dic,
+        index=uuids
+    )
+
+    ax = df.plot.barh(stacked=False)
+    plt.title(title)
+    plt.xlabel(xlabel)
+
+    x_offset = 0
+    y_offset = 0.02
+    for p in ax.patches:
+        b = p.get_bbox()
+        val = "{:.2f}".format(b.x1 - b.x0)
+        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
+
     plt.show()
 
 
 def plot_time(df):
-    values = df.loc[df.measurement_type == "Time"].value.values
-    label = df.loc[df.measurement_type == "Time"].description.values
 
-    df = pd.DataFrame(
-        dict(zip(label, values.astype(np.float))),
-        index={"Runtime"}
-    )
-
-    ax = df.plot.barh(stacked=True)
-    plt.title("Time spent in phases")
-    plt.xlabel("Time in seconds")
-    x_offset = 0
-    y_offset = 0.02
-    for p in ax.patches:
-        b = p.get_bbox()
-        print(b)
-        val = "{:.2f}".format(b.x1 - b.x0)
-        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
-    plt.show()
+    plot_barh(df, "Time", "Time spent in phases", "Time in seconds")
 
 
 def plot_throughput(df):
-    values = df.loc[df.measurement_type == "Throughput"].value.values
-    label = df.loc[df.measurement_type == "Throughput"].description.values
 
-    df = pd.DataFrame(
-        dict(zip(label, values.astype(np.float))),
-        index={"Throughput"}
-    )
-    ax = df.plot.barh()
-    plt.title("Throughput")
-    plt.xlabel("Seconds per entry")
-    x_offset = 0
-    y_offset = 0.02
-    for p in ax.patches:
-        b = p.get_bbox()
-        print(b)
-        val = "{:.2f}".format(b.x1 - b.x0)
-        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
-    plt.show()
+    plot_barh(df, "Throughput", "Throughput", "Seconds per entry")
 
 
 def plot_latency(df):
-    values = df.loc[df.measurement_type == "Latency"].value.values
-    label = df.loc[df.measurement_type == "Latency"].description.values
 
-    df = pd.DataFrame(
-        dict(zip(label, values.astype(np.float))),
-        index={"Latency"}
-    )
-    ax = df.plot.barh()
-    plt.title("Latency")
-    plt.xlabel("Entries per second")
-    x_offset = 0
-    y_offset = 0.02
-    for p in ax.patches:
-        b = p.get_bbox()
-        print(b)
-        val = "{:.2f}".format(b.x1 - b.x0)
-        ax.annotate(val, ((b.x0 + b.x1) / 2 + x_offset, b.y1 + y_offset))
-    plt.show()
+    plot_barh(df, "Latency", "Latency", "Entries per second")
+
+
+metrics_dict = {"Time": plot_time,
+                "TTA": plot_TTA,
+                "Memory": plot_memory,
+                "Energy": plot_energy,
+                "Multiclass Confusion Matrix": plot_confusion_matrix,
+                "Latency": plot_latency,
+                "Throughput": plot_throughput}
 
 
 if __name__ == "__main__":
-    visualize('aca3920c-2a79-4a6e-bb88-62b2c382e27c')   ## Willi
-    # visualize("a8c2115c-0d6a-4cbc-ad47-445289d136fc") ## Jonas
-    # visualize("e9bc18ae-eb3e-4268-beef-0c6e8e43c00a") ## Christian
+    visualize(["c147975f-8a5e-4a90-8f01-9bc62e63f33b", "c01850e8-8ab7-4819-9074-f2ea132317b4"], "so2sat_benchmark.db")
