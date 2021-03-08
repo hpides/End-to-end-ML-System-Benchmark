c04e39f9fcfcedf2ff806da54ba6e3eabef4d589
sjoze <49590442+sjoze@users.noreply.github.com>
2021-03-08 13:59:21 +0100

    Switched import and sys.path.insert

--- MNIST_pipeline/benchmarking.py
+++ MNIST_pipeline/benchmarking.py
@@ -1,6 +1,6 @@
 import os
 import sys
+sys.path.insert(0, os.getcwd())
 import package as pkg
 
-sys.path.insert(0, os.getcwd())
 bm = pkg.Benchmark('MNIST_benchmark.db', description="Complete MNIST MLP data benchmark")
