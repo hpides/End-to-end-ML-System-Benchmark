bbeeee4ed0391be5113b8a58bd29c32a88f0cb82
sjoze <49590442+sjoze@users.noreply.github.com>
2021-03-08 13:59:56 +0100

    Switched import and sys.path.insert

--- so2sat_pipeline/benchmarking.py
+++ so2sat_pipeline/benchmarking.py
@@ -1,6 +1,6 @@
 import os
 import sys
+sys.path.insert(0, os.getcwd())
 import package as pkg
 
-sys.path.insert(0, os.getcwd())
 bm = pkg.Benchmark('so2sat_benchmark.db', description="So2Sat image prediction benchmark")
