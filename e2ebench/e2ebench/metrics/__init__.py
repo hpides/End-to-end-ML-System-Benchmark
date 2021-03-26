1f1e89c36559325949834757c9a26ed45dabe10f
Jost Morgenstern <jost_morgenstern@web.de>
2021-03-26 16:16:24 +0100

    add visualization for hyperparameters

--- e2ebench/e2ebench/metrics/__init__.py
+++ e2ebench/e2ebench/metrics/__init__.py
@@ -1,8 +1,8 @@
-from e2ebench.metrics.valued_metrics import ConfusionMatrixTracker, HyperparameterTracker
+from e2ebench.metrics.valued_metrics import ConfusionMatrixTracker, HyperparameterTracker, ConfusionMatrixVisualizer, HyperparameterVisualizer
 from e2ebench.metrics.supervisor import BenchmarkSupervisor, TimeMetric, MemoryMetric
 
 
 measurement_type_mapper = {
-    "confusion-matrix" : ConfusionMatrixTracker,
-    "hyperparameters" : HyperparameterTracker
+    "confusion-matrix" : ConfusionMatrixVisualizer,
+    "hyperparameters" : HyperparameterVisualizer
 }
