234d56569596179c1ff3caf89b98443ab59d7eb0
sjoze <49590442+sjoze@users.noreply.github.com>
2021-04-08 04:42:57 +0200

    added more metrics and visualizers

--- e2ebench/e2ebench/metrics/__init__.py
+++ e2ebench/e2ebench/metrics/__init__.py
@@ -1,8 +1,15 @@
-from e2ebench.metrics.valued_metrics import ConfusionMatrixTracker, HyperparameterTracker, ConfusionMatrixVisualizer, HyperparameterVisualizer
-from e2ebench.metrics.supervised_metrics import BenchmarkSupervisor, TimeMetric, MemoryMetric, PowerMetric, EnergyMetric
+from e2ebench.metrics.valued_metrics import ConfusionMatrixTracker, HyperparameterTracker, ConfusionMatrixVisualizer, \
+    HyperparameterVisualizer
+from e2ebench.metrics.supervised_metrics import BenchmarkSupervisor, TimeMetric, MemoryMetric, PowerMetric, \
+    EnergyMetric, TimeVisualizer, MemoryVisualizer, PowerVisualizer, EnergyVisualizer
+
 
 
 measurement_type_mapper = {
     "confusion-matrix" : ConfusionMatrixVisualizer,
-    "hyperparameters" : HyperparameterVisualizer
+    "hyperparameters" : HyperparameterVisualizer,
+    "time": TimeVisualizer,
+    "memory": MemoryVisualizer,
+    "power": PowerVisualizer,
+    "energy": EnergyVisualizer
 }
