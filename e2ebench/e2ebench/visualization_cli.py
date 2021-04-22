7ab1fcfb514abee81e15caa18616e00e4d577dec
sjoze <49590442+sjoze@users.noreply.github.com>
2021-04-22 07:57:32 +0200

    works for multiple uuids, passed metadata
    works fine with either
    -u *uuid*
    or
    -u "*uuid1*, *uuid2*,... "

--- e2ebench/e2ebench/visualization_cli.py
+++ e2ebench/e2ebench/visualization_cli.py
@@ -5,6 +5,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine, asc
 
 from e2ebench.datamodel import Measurement
+from e2ebench.datamodel import BenchmarkMetadata
 from e2ebench import metrics
 
 parser = argparse.ArgumentParser(description="Visualization CLI for End to End ML System Benchmark")
@@ -22,21 +23,48 @@ def main():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    query_results = session.query(Measurement).filter_by(
-                                                benchmark_uuid=args.uuid,
-                                                measurement_type=args.type,
-                                                description=args.desc).all()
+    uuids = args.uuid.replace(" ","").split(",")
+    print(uuids)
+
+    measure_query = session.query(Measurement.benchmark_uuid,
+                                  Measurement.datetime,
+                                  Measurement.description,
+                                  Measurement.measurement_type,
+                                  Measurement.value,
+                                  Measurement.unit)
+
+    meta_query = session.query(BenchmarkMetadata.uuid,
+                               BenchmarkMetadata.description,
+                               BenchmarkMetadata.start_time)
+
+    filtered_measure_query = measure_query.filter_by(benchmark_uuid=uuids[0],
+                                                     measurement_type=args.type,
+                                                     description=args.desc)
+    filtered_meta_query = meta_query.filter_by(uuid=uuids[0])
+    for uuid in range(1, len(uuids)):
+        temp_measure_query = measure_query.filter_by(benchmark_uuid=uuids[uuid],
+                                                     measurement_type=args.type,
+                                                     description=args.desc)
+        temp_meta_query = meta_query.filter_by(uuid=uuids[uuid])
+        filtered_measure_query = filtered_measure_query.union(temp_measure_query)
+        filtered_meta_query = filtered_meta_query.union(temp_meta_query)
                     
-    if len(query_results) == 0:
+    if len(filtered_measure_query.all()) == 0:
         raise Exception("No entries found in database with given uuid, type and description")
 
-    if len(query_results) > 1:
-        raise Exception("Given combination of uuid, type and description is not unique")
+    values = []
+    for result in filtered_measure_query.all():
+        values.append(result.value)
 
-    query_result = query_results.pop()
-    Visualizer_Class = metrics.measurement_type_mapper[query_result.measurement_type]
-    measurement = Visualizer_Class(query_result.value)
-    measurement.visualize(args.uuid, args.desc)
+    starts = []
+    for result in filtered_meta_query.all():
+        starts.append(result.start_time)
+
+    filtered_measure_query = filtered_measure_query.all().pop()
+
+    Visualizer_Class = metrics.measurement_type_mapper[args.type]
+    measurement = Visualizer_Class(values)
+    measurement.visualize(uuids, args.desc, starts)
 
 if __name__ == "__main__":
     main()
