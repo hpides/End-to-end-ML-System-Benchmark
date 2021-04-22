a3d8fd0ecef9bf9a76f921be381546bd34c2ca34
sjoze <49590442+sjoze@users.noreply.github.com>
2021-04-22 08:36:40 +0200

    Create data_preparation.py

---
+++ pipelines/clothing_pipeline/data_preparation.py
@@ -0,0 +1,14 @@
+import tensorflow as tf
+
+
+def prep_data():
+    fashion_mnist = tf.keras.datasets.fashion_mnist
+
+    (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+    train_images = train_images / 255.0
+    test_images = test_images / 255.0
+
+    return train_images, test_images, train_labels, test_labels
